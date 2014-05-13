9ed06c6e07b508663ae6f3ee0fd1621fd4b9a504
Shaun Warman <shaunwarman1@gmail.com>
2014-05-13 01:55:00 -0500

    Adding old files

---
+++ src/com/example/newsreader/NewsArticle.java
@@ -0,0 +1,33 @@
+package com.example.newsreader;
+
+public class NewsArticle {
+	private String title;
+	private String summary;
+	private String source;
+	
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	
+	public String getSummary() {
+		return this.summary;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getSource() {
+		return this.source;
+	}
+	
+}
